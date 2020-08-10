76d899c7cfd3c59d92195c58eff3eae5a32319e7
Dream <dream.thoranin@gmail.com>
2020-08-10 14:37:41 +0700

    Add project files.

---
+++ Challenge_loop_if_basicmath/Challenge.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int x = 0, y = 0, a = 0;
+	float av = 0, sd = 0, sum = 0, bsd = 0;
+	scanf("%d%d", &x, &y);
+	if (x > y) {
+		for (int i = x; i >= y; i--)
+		{
+			printf("%d ", i);
+			sum = sum + i;
+			a++;
+			bsd += (i * i);
+		}
+		printf("\n");
+		av = sum / a;
+		bsd /= a;
+	}
+	else {
+		for (int i = x; i <= y; i++) {
+			printf("%d ", i);
+			sum = sum + i;
+			a++;
+			bsd += (i * i);
+		}
+		printf("\n");
+		av = sum / a;
+		bsd /= a;
+	}
+	printf("Average = %.1f\n", av);
+	av *= av;
+	printf("SD = %.2f", sqrt(bsd - av));
+	return 0;
+}
